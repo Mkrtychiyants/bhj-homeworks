1cf5f3beb8c00bcbfe121a9ad72b8042d7e616fb
Mkrtychiyants Stas <heisaloosershesad@gmail.com>
2022-11-11 20:50:13 +0300

    v2

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,24 +1,17 @@
-let cookiePict = document.getElementById("cookie");
-let clsSpan = document.getElementById("clicker__counter");
-let velSpan = document.getElementById("clicker__vel");
+const cookiePict = document.getElementById("cookie");
+const clsSpan = document.getElementById("clicker__counter");
+const velSpan = document.getElementById("clicker__vel");
 let n = 0;
 let clk = Number(clsSpan.textContent);
 let start = Date.now();
 
 let zoomerToggle = function () {
 
-    if (n === 0) {
-        cookiePict.width *= 1.5;
-        cookiePict.height *= 1.5;
-        n = 1;
-        clk++;
-    }
-    else {
-        cookiePict.width /= 1.5;
-        cookiePict.height /= 1.5;
-        n = 0;
-        clk++;
-    }
+    cookiePict.width = (n === 0) ? cookiePict.width *= 1.5 : cookiePict.width /= 1.5;
+    cookiePict.height = (n === 0) ? cookiePict.height *= 1.5 : cookiePict.height /= 1.5;
+    n = (n === 0) ? 1 : 0;
+    clk++;
+
     clsSpan.textContent = clk;
 
 
