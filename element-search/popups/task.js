4cd9386829d8aeceead0f2f6b2122fd09be10126
Mkrtychiyants Stas <heisaloosershesad@gmail.com>
2022-11-17 15:18:32 +0300

    second version

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,20 +1,23 @@
 const modMain = document.getElementById("modal_main");
-modMain.style.display = "block";
 const modals = document.getElementsByClassName("modal");
 const modCloses = document.querySelectorAll("div.modal__close")
-const showSuc = document.getElementsByClassName("show-success");
+const showSuc = document.querySelector(".show-success");
 const modSuc = document.getElementById("modal_success");
+
+modMain.classList.add("modal_active");
 
 
 function closeMod() {
     let cross = this;
-    console.log(cross.parentElement.parentElement.style.display = "none");
+    cross.closest("div.modal").classList.remove("modal_active");
 }
 
 for (let index = 0; index < modals.length; index++) {
     modCloses.item(index).onclick = closeMod;
 }
 
-showSuc.item(0).onclick = () => {
-    modSuc.style.display = "block"
+showSuc.onclick = () => {
+    let schowSucParent = showSuc.closest("div.modal");
+    schowSucParent.nextElementSibling.classList.add("modal_active");
+    schowSucParent.classList.remove("modal_active");
 }
